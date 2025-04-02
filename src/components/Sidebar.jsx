eb9454d133e5f3121fd48a6605649dbbcf083ecc
fadialjohari0 <fadialjohari0@gmail.com>
2025-04-02 16:37:20 +0300

    Highlight selected sidebar link - Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Menu } from 'lucide-react';
 import { SIDEBAR_LINKS } from '../constants/SidebarLinks.const';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
+  const location = useLocation();
+
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
@@ -20,25 +22,30 @@ const Sidebar = () => {
           <Menu size={24} />
         </motion.button>
         <nav className='mt-8 flex-grow'>
-          {SIDEBAR_LINKS?.map((item, index) => (
-            <Link key={item?.href} to={item?.href}>
-              <motion.div className='flex items-center p-4 text-sm font-medium rounded-lg hover:bg-gray-700 transition-colors mb-2'>
-                <item.icon size={20} style={{ color: item?.color, minWidth: '20px' }} />
-                <AnimatePresence>
-                  {isSidebarOpen && (
-                    <motion.span
-                      className='ml-4 whitespace-nowrap'
-                      initial={{ opacity: 0, width: 0 }}
-                      animate={{ opacity: 1, width: 'auto' }}
-                      exit={{ opacity: 0, width: 0 }}
-                      transition={{ duration: 0.2, delay: 0.3 }}>
-                      {item?.name}
-                    </motion.span>
-                  )}
-                </AnimatePresence>
-              </motion.div>
-            </Link>
-          ))}
+          {SIDEBAR_LINKS?.map((item, index) => {
+            const isActive = location.pathname === item.href;
+
+            return (
+              <Link key={item?.href} to={item?.href}>
+                <motion.div
+                  className={`flex items-center p-4 text-sm font-medium rounded-lg transition-colors mb-2 ${isActive ? 'bg-gray-700' : 'hover:bg-gray-700'}`}>
+                  <item.icon size={20} style={{ color: item?.color, minWidth: '20px' }} />
+                  <AnimatePresence>
+                    {isSidebarOpen && (
+                      <motion.span
+                        className='ml-4 whitespace-nowrap'
+                        initial={{ opacity: 0, width: 0 }}
+                        animate={{ opacity: 1, width: 'auto' }}
+                        exit={{ opacity: 0, width: 0 }}
+                        transition={{ duration: 0.2, delay: 0.3 }}>
+                        {item?.name}
+                      </motion.span>
+                    )}
+                  </AnimatePresence>
+                </motion.div>
+              </Link>
+            );
+          })}
         </nav>
       </div>
     </motion.div>
