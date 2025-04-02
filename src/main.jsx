1b2441219ef10e4e83345a0b32243464bc5469a8
fadialjohari0 <fadialjohari0@gmail.com>
2025-04-02 17:00:34 +0300

    Add vercel analytics package

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+
+import { Analytics } from '@vercel/analytics/react';
+
+import App from './App.jsx';
+
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 );
